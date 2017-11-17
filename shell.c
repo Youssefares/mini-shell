46d431f40f6e14faa498686b43e62db56c549c20
Youssefares <Youssefares@ymail.com>
2017-11-18 00:20:55 +0200

    Modify to execute commands correctly in child processes, Factor out tokenization, Integrate cd

--- shell.c
+++ shell.c
@@ -3,22 +3,27 @@
 #include <string.h>
 #include <strings.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #define MAX_COMMAND_LENGTH 512
 #define KGRN  "\x1B[32m"
 #define KWHT  "\x1B[37m"
 
 char *input_string(FILE*, size_t);
+char** array_of_tokens(char*, char*, bool*);
 
 
 int main(void)
 {
   char* s = malloc(sizeof(char)*(MAX_COMMAND_LENGTH+1));
+  pid_t pid;
   while(true){
     printf("%s$mini-shell> %s", KGRN, KWHT);
     s = input_string(stdin, MAX_COMMAND_LENGTH+1);
     if(s == NULL){
-      fprintf(stderr, "Command too long, use <= %i chars\n", MAX_COMMAND_LENGTH);
+      fprintf(stderr, "Error: Command too long, use <= %i chars\n", MAX_COMMAND_LENGTH);
       continue;
     }
 
@@ -31,22 +36,30 @@ int main(void)
       continue;
     }
 
-    int i = 0;
-    int size = 2;
-    char** args = realloc(NULL, sizeof(char*)*size);
-    char* pch = strtok (s," ");
-    args[i++] = pch;
-    while(pch != NULL)
-    {
-      pch = strtok (NULL, " ");
-      args[i++] = pch;
-      if (i == size){
-        args = realloc(args, sizeof(char*)*(size*=2));
+    bool background = false;
+    char** args = array_of_tokens(s, " ", &background);
+    if (strcmp(args[0], "cd") == 0){
+      if(args[1] == NULL || args[2] != NULL){
+        fprintf(stderr, "Error: wrong number of arguments\n");
+        continue;
       }
+      if(chdir(args[1]) == -1){
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+      }
+      continue;
     }
-    args = realloc(args, sizeof(char*)*i--);
-    for(int j = 0; j < i; j++){
-      printf("%i %s\n", j, args[j]);
+
+    pid = fork();
+    if(pid == 0){
+      if(execvp(args[0], args)){
+        perror(strerror(errno));
+      }
+      return 0;
+    } else {
+      if(!background){
+        waitpid(pid, NULL, 0);
+      }
+      continue;
     }
   }
   return -1;
@@ -58,7 +71,7 @@ char *input_string(FILE* fp, size_t max){
     int ch;
     size_t size = 5;
     size_t len = 0;
-    str = realloc(NULL, sizeof(char)*size);//size is start size
+    str = realloc(NULL, sizeof(char)*size);
     if(!str)return str;
     while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
         if(len == max){
@@ -75,4 +88,26 @@ char *input_string(FILE* fp, size_t max){
     }
     str[len++]='\0';
     return realloc(str, sizeof(char)*len);
+}
+
+
+char** array_of_tokens(char* s, char* terminators, bool* background){
+    int i = 0;
+    int size = 2;
+    char** args = realloc(NULL, sizeof(char*)*size);
+    char* pch = strtok (s, terminators);
+    args[i++] = pch;
+    while(pch != NULL)
+    {
+      pch = strtok (NULL, terminators);
+      args[i++] = pch;
+      if (i == size){
+        args = realloc(args, sizeof(char*)*(size*=2));
+      }
+    }
+    if(strcmp(args[i-2], "&") == 0){
+      *background = true;
+      args[i-2] = "";
+    }
+    return realloc(args, sizeof(char*)*i);
 }
