b73c9488d0a94f95c844b7a7df2a0ea86ed0eee6
Youssefares <Youssefares@ymail.com>
2017-11-17 22:32:42 +0200

    Add color + tokenization by space

--- shell.c
+++ shell.c
@@ -4,13 +4,18 @@
 #include <strings.h>
 #include <stdbool.h>
 
-#define MAX_COMMAND_LENGTH 3
+#define MAX_COMMAND_LENGTH 512
+#define KGRN  "\x1B[32m"
+#define KWHT  "\x1B[37m"
+
 char *input_string(FILE*, size_t);
+
 
 int main(void)
 {
   char* s = malloc(sizeof(char)*(MAX_COMMAND_LENGTH+1));
   while(true){
+    printf("%s$mini-shell> %s", KGRN, KWHT);
     s = input_string(stdin, MAX_COMMAND_LENGTH+1);
     if(s == NULL){
       fprintf(stderr, "Command too long, use <= %i chars\n", MAX_COMMAND_LENGTH);
@@ -25,7 +30,24 @@ int main(void)
     if (strcmp(s, "") == 0) {
       continue;
     }
-    printf("%s\n", s);
+
+    int i = 0;
+    int size = 2;
+    char** args = realloc(NULL, sizeof(char*)*size);
+    char* pch = strtok (s," ");
+    args[i++] = pch;
+    while(pch != NULL)
+    {
+      pch = strtok (NULL, " ");
+      args[i++] = pch;
+      if (i == size){
+        args = realloc(args, sizeof(char*)*(size*=2));
+      }
+    }
+    args = realloc(args, sizeof(char*)*i--);
+    for(int j = 0; j < i; j++){
+      printf("%i %s\n", j, args[j]);
+    }
   }
   return -1;
 }
