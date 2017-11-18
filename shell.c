1086ffbef1897c5547bea24aced9d01372eff054
Youssefares <Youssefares@ymail.com>
2017-11-18 12:48:42 +0200

    Fix spaces only input

--- shell.c
+++ shell.c
@@ -11,8 +11,8 @@
 #define KGRN  "\x1B[32m"
 #define KWHT  "\x1B[37m"
 
-char *input_string(FILE*, size_t);
-char** array_of_tokens(char*, char*, bool*);
+char* input_string(FILE*, size_t);
+char** array_of_tokens(char*, char*, bool*); 
 
 
 int main(void)
@@ -27,6 +27,7 @@ int main(void)
       continue;
     }
 
+
     if(strcasecmp(s, "exit") == 0){
       printf("Exiting..\n");
       return 0;
@@ -38,6 +39,9 @@ int main(void)
 
     bool background = false;
     char** args = array_of_tokens(s, " ", &background);
+    if (args == NULL){
+      continue;
+    }
     if (strcmp(args[0], "cd") == 0){
       if(args[1] == NULL || args[2] != NULL){
         fprintf(stderr, "Error: wrong number of arguments\n");
@@ -72,16 +76,24 @@ char *input_string(FILE* fp, size_t max){
     size_t size = 5;
     size_t len = 0;
     str = realloc(NULL, sizeof(char)*size);
+    bool c_inputted = false;
     if(!str)return str;
+
     while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
-        if(len == max){
-          continue;
-        }
-        str[len++]=ch;
-        if(len==size){
-            str = realloc(str, sizeof(char)*(size+=16));
-            if(!str)return str;
-        }
+      if (!c_inputted && (ch == ' ' || ch == '\n')){
+        continue;
+      }
+      if (ch != ' '){
+        c_inputted = true;
+      }
+      if(len == max){
+        continue;
+      }
+      str[len++]=ch;
+      if(len==size){
+          str = realloc(str, sizeof(char)*(size+=16));
+          if(!str)return str;
+      }
     }
     if(len >= max){
       return NULL;
@@ -105,6 +117,7 @@ char** array_of_tokens(char* s, char* terminators, bool* background){
         args = realloc(args, sizeof(char*)*(size*=2));
       }
     }
+
     if(strcmp(args[i-2], "&") == 0){
       *background = true;
       args[i-2] = "";
